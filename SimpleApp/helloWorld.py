5e843922813441bfa6adbc20f5012f036f233c00
pradyumn.v <pradyumn.vishramverma@appviewx.com>
2025-01-09 00:33:20 +0530

    new commit

--- SimpleApp/helloWorld.py
+++ SimpleApp/helloWorld.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import requests
 
 # Create a Flask application instance
 app = Flask(__name__)
@@ -11,7 +12,9 @@ def hello_world():
 # Define a route to render the atheism page
 @app.route('/atheism')
 def atheism():
-    return render_template('atheism.html')
+    response = requests.get("https://api.chucknorris.io/jokes/random")
+    joke = response.json()["value"]
+    return render_template('atheism.html', text=joke)
 
 # Run the app if this script is executed directly
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,1 +1,10 @@
+import requests
 
+# # Age guessing API
+# name = input("Enter your name: ")
+# response = requests.get(f"https://api.agify.io/?name={name}")
+# print(response.json())
+
+# # Joke API
+response = requests.get("https://api.chucknorris.io/jokes/random")
+print(response.json()["value"])
