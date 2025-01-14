967775f069aba5126e530f426b0df7c7ebce60d7
pradyumn.v <pradyumn.vishramverma@appviewx.com>
2025-01-14 12:41:28 +0530

    OMDB commit

--- SimpleApp/helloWorld.py
+++ SimpleApp/helloWorld.py
@@ -16,6 +16,33 @@ def atheism():
     joke = response.json()["value"]
     return render_template('atheism.html', text=joke)
 
+
+# Define a route for the movie search page
+@app.route('/movie', methods=['GET', 'POST'])
+def movie():
+    OMDB_API_KEY = '3bfcdb1'
+    movie_data = None  # Initialize the variable to store movie details
+    error_message = None
+
+    if request.method == 'POST':
+        movie_title = request.form.get('title', '').strip()
+
+        if not movie_title:
+            error_message = "Please enter a movie title."
+        else:
+            # Call the OMDB API
+            url = f'https://www.omdbapi.com/?apikey={OMDB_API_KEY}&t={movie_title}'
+            response = requests.get(url)
+            data = response.json()
+
+            if data.get('Response') == 'True':
+                movie_data = data
+            else:
+                error_message = "Movie not found. Please try again."
+
+    return render_template('movie.html', movie_data=movie_data, error_message=error_message)
+
+
 # Run the app if this script is executed directly
 if __name__ == '__main__':
     app.run(debug=True)
