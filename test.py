f3af80244451f32c9d43275a1d3a9840cf5e6895
pradyumn.v <pradyumn.vishramverma@appviewx.com>
2025-01-14 12:50:19 +0530

    Movie API added

--- test.py
+++ test.py
@@ -5,6 +5,10 @@ import requests
 # response = requests.get(f"https://api.agify.io/?name={name}")
 # print(response.json())
 
-# Joke API
-response = requests.get("https://api.chucknorris.io/jokes/random")
-print(response.json()["value"])
+# # Joke API
+# response = requests.get("https://api.chucknorris.io/jokes/random")
+# print(response.json()["value"])
+
+# OMDB API
+response = requests.get("https://www.omdbapi.com/?apikey=xxxxx&t=Deewaar&plot=full")
+print(response.json()['Plot'])
